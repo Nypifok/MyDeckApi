3413d98c7474379d3f77d17bdc1c6689ebbd23fc
Nypifok <53980718+Nypifok@users.noreply.github.com>
2020-03-09 19:51:13 +0300

    Tests

---
+++ MyDeckAPI.Tests/UserControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using MyDeckAPI.Controllers;
+using MyDeckAPI.Interfaces;
+using MyDeckAPI.Models;
+using NUnit.Framework;
+
+namespace MyDeckAPI.Tests
+{
+    [TestFixture]
+    class UserControllerTest
+    {
+        [Test]
+        public void FindAllTest()
+        {
+            var mock = new Mock<IGenericRepository<User>>();
+
+
+            var mock1 = new Mock<ILogger<UserController>>();
+            UserController controller = new UserController(mock1.Object, mock.Object);
+            Assert.AreEqual(result, controller.FindAll());
+
+        }
+        
+       
+        
+    }
+}
---
+++ MyDeckAPI_Tests/UserRepository_FindAll.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using MyDeckAPI.Models;
+using MyDeckAPI.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace MyDeckAPI_Tests
+{
+    [TestFixture]
+    class UserRepository_FindAll
+    {
+        [Test]
+        public void IsFindAll_InputIsUsers_ReturnAllUsers()
+        {
+            
+            
+        }
+    }
+}
